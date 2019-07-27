d497646137639519000aab1e9ac319f4a617e65a
Antonio Zapata <48960021+antoniozapata@users.noreply.github.com>
2019-07-27 20:18:22 +0200

    menu de navegacion

--- scripts/scriptproductos.js
+++ scripts/scriptproductos.js
@@ -10,30 +10,70 @@ const products = [{
     code: "NAXIP",
     productName: "Ajuste por Inflación",
     url: "./products-axi.html",
+}, {
+    img: "../resources/productos/cont1.png",
+    imgFlag: "../resources/productos/bandera_ven.png",
+    code: "NCONL",
+    productName: "Contabilidad General",
+    url: "./products-contabil.html",
+}, {
+    img: "../resources/productos/der.png",
+    imgFlag: "../resources/productos/bandera_ven.png",
+    code: "NDERL",
+    productName: "Declaraciones",
+    url: "./products-der.html",
+}, {
+    img: "../resources/productos/iva.png",
+    imgFlag: "../resources/productos/bandera_ven.png",
+    code: "NIVAL",
+    productName: "Impuesto al Valor Agregado",
+    url: "./products-iva.html",
+}, {
+    img: "../resources/productos/nom.png",
+    imgFlag: "../resources/productos/bandera_ven.png",
+    code: "NNOMP",
+    productName: "Nomina Profesional",
+    url: "./products-nom.html",
+}, {
+    img: "../resources/productos/ret.png",
+    imgFlag: "../resources/productos/bandera_ven.png",
+    code: "NRET",
+    productName: "Retenciones",
+    url: "./products-ret.html",
 }];
 
 products.forEach((prod) => {
     const listItem = document.createElement("li");
+    listItem.setAttribute("class", "container-ficha-sistema");
+
     const containerImgSistema = document.createElement("div");
+    containerImgSistema.setAttribute("class", "container-img-sistema");
+
     const fichaProducto = document.createElement("div");
+
     const linkTo = document.createElement("a");
+    linkTo.setAttribute("href", prod.url);
+    linkTo.setAttribute("class", "");
+
     const imgProd = document.createElement("img");
-    const imgFlag = document.createElement("img")
+    imgProd.setAttribute("src", prod.img);
+    imgProd.setAttribute("alt", "");
+    imgProd.setAttribute("class", "galac-iconoproducto")
+
+    const imgFlag = document.createElement("img");
+    imgFlag.setAttribute("src", prod.imgFlag);
+    imgFlag.setAttribute("alt", "");
+    imgFlag.setAttribute("class", "galac_bandera-ven")
+
     const productCode = document.createElement("p");
+
     const moreInfo = document.createElement("p");
+    moreInfo.setAttribute("class", "boton-masinfo")
+    
     const productName = document.createElement("h3");
 
     /*linkTo.href = prod.url este se utiliza para cambiar el valor del atributo*/
-    linkTo.setAttribute("href", prod.url);
-    linkTo.setAttribute("class", "container-ficha-sistema");
-
-    imgProd.setAttribute("src", prod.img);
-    imgProd.setAttribute("alt", "");
-
-    imgFlag.setAttribute("src", prod.imgFlag);
-    imgFlag.setAttribute("alt", "");
-    
-    const ul = document.querySelector(".galac_listadeprogramas ul");
+    const ul = document.querySelector(".products-section ul");
     ul.appendChild(listItem);
 
     listItem.appendChild(linkTo);
@@ -48,7 +88,4 @@ products.forEach((prod) => {
     productCode.innerHTML = prod.code;
     productName.innerHTML = prod.productName;
     moreInfo.innerHTML = "Más Información";
-    
-
-    
 })
