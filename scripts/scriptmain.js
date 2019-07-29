cd17ac4ea1da03c4934182183ecb2b1cf614028c
Victoria Muñoz <victoriamunozsisso@gmail.com>
2019-07-29 22:59:39 +0200

    cambios productos main

--- scripts/scriptmain.js
+++ scripts/scriptmain.js
@@ -1,5 +1,39 @@
-window.addEventListener ("load", function () {
-    this.console.log("El contenido ha cargado")
+const productsHome = [{
+    img:"../resources/productos/admin.png",
+    productName: "Administrativo",
+}, {
+    img:"../resources/productos/axi.png",
+    productName: "Ajuste por Inflacion",
+}, {
+    img:"../resources/productos/cont1.png",
+    productName: "Contabilidad General"
+}, {
+    img:"../resources/productos/der.png",
+    productName: "Declaraciones"
+}, {
+    img:"../resources/productos/iva.png",
+    productName:"Iva y Renta"
+}, {
+    img:"../resources/productos/nom.png",
+    productName:"Nomina Profesional"
+}];
 
-    this.document.slider.src = ""
-})
+products.forEach((prod) => {
+    const listProd = document.createElement("li");
+
+    const imgProdHome = document.createElement("img");
+    imgProdHome.setAttribute("src", prod.img);
+    imgProdHome.setAttribute("alt", "");
+    imgProdHome.setAttribute("class", "galac-iconoproducto");
+
+    const productName = document.createElement("p");
+    productName.setAttribute("class", "boton");
+
+    const ul = document.querySelector(".container_productos");
+    ul.appendChild(listProd);
+
+    listProd.appendChild(imgProdHome);
+    listProd.appendChild(productName);
+
+    productName.innerHTML = prod.productName;
+});
